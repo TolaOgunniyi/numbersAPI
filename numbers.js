b7e6e36e353df95c1e0d52efd2670894f4f63b4d
Tola Ogunniyi <tola.ogunniyi1@gmail.com>
2025-05-07 13:04:08 -0600

    CSS styles added for the body of the page

--- numbers.js
+++ numbers.js
@@ -2,7 +2,7 @@ const mathTrivia = async () =>{
     const response = await fetch("http://numbersapi.com/random?json");
     const data = await response.json();
     console.log(data.text)
-    return data.text
+    document.querySelector('.displayFacts').textContent = data.text;
 }
 
-document.querySelector('.displayFacts').innerText = mathTrivia();
+mathTrivia()
